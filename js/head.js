c9e541b1dc38392f0a1d095bc773849c48937d0c
seeys <es2008e@naver.com>
2022-04-20 21:33:14 +0900

    head, clock, inforbox 수정

--- js/head.js
+++ js/head.js
@@ -1,122 +1,118 @@
 /*시계 담당*/
 
-function getDate(){
-    let today = new Date();
+function getDate() {
+  let today = new Date();
 
-    let hour= today.getHours();
-    if(hour < 10) hour = '0'+ hour;
+  let hour = today.getHours();
+  if (hour < 10) hour = "0" + hour;
 
-    let min = today.getMinutes();
-    if(min < 10) min = '0'+ min;
+  let min = today.getMinutes();
+  if (min < 10) min = "0" + min;
 
-    let sec = today.getSeconds();
-    if(sec < 10) sec = '0'+ sec;
+  let sec = today.getSeconds();
+  if (sec < 10) sec = "0" + sec;
 
-    let str = `${today.getFullYear()}년 ${today.getMonth()+1}월 ${today.getDate()}일 ${hour}시 ${min}분 ${today.getSeconds()}초`
-    const text = document.querySelector(".sub-header__clock span:nth-child(2)")
-    text.innerHTML = str;
-
+  let str = `${today.getFullYear()}년 ${
+    today.getMonth() + 1
+  }월 ${today.getDate()}일 ${hour}시 ${min}분 ${today.getSeconds()}초`;
+  const text = document.querySelector(".clock-sub-header span:nth-child(3)");
+  text.innerHTML = str;
 }
 
-function getUpdateDate(){
+function getUpdateDate() {
+  let today = new Date();
+  let doc = document.querySelector(".clock-sub-header span:first-child");
 
-    let today = new Date();
-    let doc = document.querySelector(".sub-header__clock span:first-child");
-    
-    if(today.getHours() < 10){
-        let yesterday = today.getTime() - (1*24*60*60*1000);
-        today.setTime(yesterday);
-    }
-    
-    let year = today.getFullYear();
-    let month = today.getMonth()+1;
-    let date = today.getDate();
-    
-    doc.innerHTML = `(${year}년 ${month}월 ${date}일 기준 업데이트)`;
+  if (today.getHours() < 10) {
+    let yesterday = today.getTime() - 1 * 24 * 60 * 60 * 1000;
+    today.setTime(yesterday);
+  }
+
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+
+  doc.innerHTML = `(${year}년 ${month}월 ${date}일 기준 업데이트)`;
 }
 
 /* 다크모드 */
 
-function setDarkMode(){
+function setDarkMode() {
+  console.log("dark mode work");
+  document.body.style.background = "#121212";
 
-    console.log("dark mode work");
-    document.body.style.background="#121212";
+  let doc = document.querySelector(".cotitle");
+  doc.classList.add("isDark-fontcolor");
 
-    let doc = document.querySelector(".cotitle");
-    doc.classList.add("isDark-fontcolor");
-    
-    doc = document.querySelector(".sub-header__clock");
-    doc.classList.add("isDark-fontcolor");
+  doc = document.querySelector(".sub-header__clock");
+  doc.classList.add("isDark-fontcolor");
 
-    doc = document.querySelector(".information__box");
-    doc.classList.remove("bg-light");
-    doc.classList.add("bg-darkmode");
-    doc.classList.add("isDark-fontcolor");
+  doc = document.querySelector(".information__box");
+  doc.classList.remove("bg-light");
+  doc.classList.add("bg-darkmode");
+  doc.classList.add("isDark-fontcolor");
 
-    doc = document.querySelectorAll(".kmap__box");
+  doc = document.querySelectorAll(".kmap__box");
 
-    for(let x =0 ; x<18 ; x++){
-        doc[x].classList.remove("bg-light");
-        doc[x].classList.add("bg-darkmode");
-        doc[x].classList.add("isDark-fontcolor");
-    }
+  for (let x = 0; x < 18; x++) {
+    doc[x].classList.remove("bg-light");
+    doc[x].classList.add("bg-darkmode");
+    doc[x].classList.add("isDark-fontcolor");
+  }
 
-    doc = document.querySelector(".brightness");
-    doc.style.backgroundColor = "#f1f1f1";
+  doc = document.querySelector(".brightness");
+  doc.style.backgroundColor = "#f1f1f1";
 
-    localStorage.setItem("isDark","true");
-
+  localStorage.setItem("isDark", "true");
 }
 
-function setWhiteMode(){
+function setWhiteMode() {
+  console.log("white mode work");
+  document.body.style.background = "#ffffff";
 
-    console.log("white mode work");
-    document.body.style.background="#ffffff";
+  let doc = document.querySelector(".cotitle");
+  doc.classList.remove("isDark-fontcolor");
 
-    let doc = document.querySelector(".cotitle");
-    doc.classList.remove("isDark-fontcolor");
+  doc = document.querySelector(".sub-header__clock");
+  doc.classList.remove("isDark-fontcolor");
 
-    doc = document.querySelector(".sub-header__clock");
-    doc.classList.remove("isDark-fontcolor");
+  doc = document.querySelector(".information__box");
+  doc.classList.add("bg-light");
+  doc.classList.remove("bg-darkmode");
+  doc.classList.remove("isDark-fontcolor");
 
-    doc = document.querySelector(".information__box");
-    doc.classList.add("bg-light");
-    doc.classList.remove("bg-darkmode");
-    doc.classList.remove("isDark-fontcolor");
+  doc = document.querySelectorAll(".kmap__box");
+  for (let x = 0; x < 18; x++) {
+    doc[x].classList.add("bg-light");
+    doc[x].classList.remove("bg-darkmode");
+    doc[x].classList.remove("isDark-fontcolor");
+  }
 
-    doc = document.querySelectorAll(".kmap__box");
-    for(let x =0 ; x<18 ; x++){
-        doc[x].classList.add("bg-light");
-        doc[x].classList.remove("bg-darkmode");
-        doc[x].classList.remove("isDark-fontcolor");
-    }
+  doc = document.querySelector(".brightness");
+  doc.style.backgroundColor = "transparent";
 
-    doc = document.querySelector(".brightness");
-    doc.style.backgroundColor = "transparent";
-
-    localStorage.setItem("isDark","false");
+  localStorage.setItem("isDark", "false");
 }
 
-function clickButton(){
-    let loadedSet = localStorage.getItem("isDark");
-    if(loadedSet === null){
-        localStorage.setItem("isDark", "false");
-    }
+function clickButton() {
+  let loadedSet = localStorage.getItem("isDark");
+  if (loadedSet === null) {
+    localStorage.setItem("isDark", "false");
+  }
 
-    if(loadedSet === "false" ) setDarkMode();
-    else setWhiteMode();
+  if (loadedSet === "false") setDarkMode();
+  else setWhiteMode();
 }
 
-function init(){
-    const BUTTON = document.querySelector(".brightness");
-    BUTTON.addEventListener("click", clickButton);
+function init() {
+  const BUTTON = document.querySelector(".brightness");
+  BUTTON.addEventListener("click", clickButton);
 
-    if(localStorage.getItem("isDark") === "true") setDarkMode();
+  if (localStorage.getItem("isDark") === "true") setDarkMode();
 
-    getDate();
-    setInterval(getDate,1000);
-    getUpdateDate();
-
+  getDate();
+  setInterval(getDate, 1000);
+  getUpdateDate();
 }
 
 init();
--- js/today.js
+++ js/today.js
@@ -1,103 +1,104 @@
-const API_KEY = "pZs%2BQbjTvsjYdeBc40z2fD7QRRi3lynCg2GLLsEj2n8%2B1thhngNGdz5RitRWetZTfHRlD0Etu%2BGFU6rTWSmIHg%3D%3D";
+const API_KEY =
+  "pZs%2BQbjTvsjYdeBc40z2fD7QRRi3lynCg2GLLsEj2n8%2B1thhngNGdz5RitRWetZTfHRlD0Etu%2BGFU6rTWSmIHg%3D%3D";
 
-function getDate(){                                     /* 10시 전 업데이트 x 어제의 날짜 가져오기*/
-    let today = new Date();
-    
-    if(today.getHours() < 10){
-        let yesterday = today.getTime() - (1*24*60*60*1000);
-        today.setTime(yesterday);
-    }
-    
-    let year = today.getFullYear();
+function getDate() {
+  /* 10시 전 업데이트 x 어제의 날짜 가져오기*/
+  let today = new Date();
 
-    let month = today.getMonth()+1;
-    if(month < 10) month = '0'+ month;
-    
-    let date = today.getDate();
-    if(date < 10) date = '0'+ date;
+  if (today.getHours() < 10) {
+    let yesterday = today.getTime() - 1 * 24 * 60 * 60 * 1000;
+    today.setTime(yesterday);
+  }
 
-    return (`${year}${month}${date}`);
+  let year = today.getFullYear();
 
+  let month = today.getMonth() + 1;
+  if (month < 10) month = "0" + month;
+
+  let date = today.getDate();
+  if (date < 10) date = "0" + date;
+
+  return `${year}${month}${date}`;
 }
 
-function getYesterDate(){                       /* 10시 전 업데이트 x 어제의 날짜 가져오기*/
-    let today = new Date();
-    
-    if(today.getHours() < 10){
-        let yesterday = today.getTime() - (1*24*60*60*1000*2);
-        today.setTime(yesterday);
-    }
-    else{
-        let yesterday = today.getTime() - (1*24*60*60*1000);
-        today.setTime(yesterday);
-    }
-    
-    let year = today.getFullYear();
-    let month = today.getMonth()+1;
-    if(month < 10){
-        month = '0'+ month;
-    }
-    let date = today.getDate();
-    if(date < 10){
-        date = '0'+ date;
-    }
-    return (`${year}${month}${date}`);
+function getYesterDate() {
+  /* 10시 전 업데이트 x 어제의 날짜 가져오기*/
+  let today = new Date();
+
+  if (today.getHours() < 10) {
+    let yesterday = today.getTime() - 1 * 24 * 60 * 60 * 1000 * 2;
+    today.setTime(yesterday);
+  } else {
+    let yesterday = today.getTime() - 1 * 24 * 60 * 60 * 1000;
+    today.setTime(yesterday);
+  }
+
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  let date = today.getDate();
+  if (date < 10) {
+    date = "0" + date;
+  }
+  return `${year}${month}${date}`;
 }
 
-function makeUrl(date1, date2){
+function makeUrl(date1, date2) {
+  let url =
+    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson";
+  let queryParams = "?" + "ServiceKey" + "=" + API_KEY;
+  queryParams += "&" + "pageNo" + "=" + "1";
+  queryParams += "&" + "numOfRows" + "=" + "10";
+  queryParams += "&" + "startCreateDt" + "=" + `${date1}`;
+  queryParams += "&" + "endCreateDt" + "=" + `${date2}`;
 
-    let url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'; 
-    let queryParams = '?' + 'ServiceKey' + '=' + API_KEY;
-    queryParams += '&' + 'pageNo' + '=' + '1'; 
-    queryParams += '&' + 'numOfRows' + '=' + '10'; 
-    queryParams += '&' + 'startCreateDt'+ '=' + `${date1}`;
-    queryParams += '&' + 'endCreateDt' + '=' + `${date2}`;
-
-    console.log(url + queryParams);
-    return (url + queryParams);
-
+  console.log(url + queryParams);
+  return url + queryParams;
 }
 
+function ParseData(date) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", makeUrl(getYesterDate(), getDate()));
 
-function ParseData(date){
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      let td; /* 오늘 데이터 */
+      let yd; /* 어제 데이터 */
+      let doc; /* 쿼리셀렉터 */
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', makeUrl(getYesterDate(), getDate()));
+      const tag = ["decideCnt", "deathCnt", "examCnt", "clearCnt"];
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
+      for (let x = 0; x < 2; x++) {
+        td = this.responseXML.getElementsByTagName(`${tag[x]}`)[0].childNodes[0]
+          .nodeValue;
+        yd = this.responseXML.getElementsByTagName(`${tag[x]}`)[1].childNodes[0]
+          .nodeValue;
 
-            let td; /* 오늘 데이터 */
-            let yd; /* 어제 데이터 */
-            let doc; /* 쿼리셀렉터 */
-
-            const tag = ["decideCnt", "deathCnt", "examCnt","clearCnt"];
-
-            for (let x = 0 ; x<4 ; x++){
-                td = this.responseXML.getElementsByTagName(`${tag[x]}`)[0].childNodes[0].nodeValue;
-                yd = this.responseXML.getElementsByTagName(`${tag[x]}`)[1].childNodes[0].nodeValue;
-
-                doc = document.querySelector(`.information__box div:nth-child(${x+1}) span:nth-child(2)`);
-                doc.innerHTML = td;
-                doc = document.querySelector(`.information__box div:nth-child(${x+1}) span:nth-child(3)`);
-                if((td-yd)>0){
-                    doc.innerHTML = `↑ ${td-yd}`;
-                    doc.classList.add("incre-big");  /* 숫자가 0보다 크면 빨갛게 표시 */
-                }
-                else{
-                    doc.innerHTML = `- ${td-yd}`;
-                    doc.classList.add("noincre-big");
-                }
-            }
-            
+        doc = document.querySelector(
+          `.information__box div:nth-child(${x + 1}) span:nth-child(2)`
+        );
+        doc.innerHTML = td;
+        doc = document.querySelector(
+          `.information__box div:nth-child(${x + 1}) span:nth-child(3)`
+        );
+        if (td - yd > 0) {
+          doc.innerHTML = `↑ ${td - yd}`;
+          doc.classList.add("incre-big"); /* 숫자가 0보다 크면 빨갛게 표시 */
+        } else {
+          doc.innerHTML = `- ${td - yd}`;
+          doc.classList.add("noincre-big");
         }
-    };
-xhr.send('');
+        console.log(td, yd);
+      }
+    }
+  };
+  xhr.send("");
 }
 
-
-function init(){
-    ParseData();
+function init() {
+  ParseData();
 }
 
 init();
